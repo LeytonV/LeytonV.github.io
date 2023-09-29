cf528ed6628c2238d91a6f7a9f1c52d84892950e
Leyton Viner <leytonytube@gmail.com>
2023-09-29 14:00:26 +0100

    Ok something didnt work

--- Assets/Navbar/Navbar.js
+++
@@ -1,8 +0,0 @@
-const navbar = document.createElement("nav");
-var client = new XMLHttpRequest();
-client.open('GET', 'Assets/Navbar/Navbar.html');
-client.onreadystatechange = function() {
-    navbar.innerHTML = client.responseText;
-    document.body.prepend(navbar);
-}
-client.send();
